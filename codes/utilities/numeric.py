ada1603e0ae0e5832a9bac68233c7ce65d73ee61
wei_lin <wei1234c@gmail.com>
2022-02-15 01:29:28 +0800

    re-organize class hierachy.

--- codes/utilities/numeric.py
+++ codes/utilities/numeric.py
@@ -11,6 +11,17 @@ class Number:
         self.value = value
 
 
+    @property
+    def type(self):
+        return type(self.value)
+
+
+    @classmethod
+    def limit_guard(cls, value, n_bits):
+        limit = 1 << (n_bits - 1)
+        assert - limit <= value < limit, f'Need {-limit} <= value < {limit}, current: {value}'
+
+
     @classmethod
     def from_bits(cls, bits):
         raise NotImplementedError
@@ -52,76 +63,13 @@ class Number:
 
 
 
-class SignedInteger(Number):
-    N_BITS = 32
-
-
-    def __init__(self, value, n_bits = None):
-        super().__init__(value)
-        self._n_bits = n_bits or self.N_BITS
-
-        self.limit_guard(value, self._n_bits)
-
-
-    @classmethod
-    def limit_guard(cls, value, n_bits):
-        limit = 1 << (n_bits - 1)
-        assert - limit <= value < limit, f'Need {-limit} <= value < {limit}, current: {value}'
-
-
-    @classmethod
-    def from_bits(cls, bits, n_bits):
-        return cls(cls.bits_to_value(bits, n_bits), n_bits)
-
-
-    @classmethod
-    def from_bytes(cls, value_bytes, n_bits):
-        return cls(cls.bytes_to_value(value_bytes, n_bits), n_bits)
-
-
-    @classmethod
-    def bits_to_value(cls, bits, n_bits):
-        sign_mask = 1 << (n_bits - 1)
-        return (bits & (sign_mask - 1)) - \
-               (bits & (sign_mask - 0))
-
-
-    @classmethod
-    def bytes_to_value(cls, value_bytes, n_bits):
-        bits = int.from_bytes(value_bytes, byteorder = 'big', signed = False)
-        return cls.bits_to_value(bits, n_bits)
-
-
-    @classmethod
-    def to_bits(cls, value, n_bits):
-        return value + 2 ** n_bits if value < 0 else value
-
-
-    @classmethod
-    def to_bytes(cls, value, n_bits):
-        bits = cls.to_bits(value, n_bits)
-        return bits.to_bytes(length = ceil(n_bits / 8),
-                             byteorder = 'big',
-                             signed = False)
-
-
-    @property
-    def bits(self):
-        return self.to_bits(self.value, self._n_bits)
-
-
-    @property
-    def bytes(self):
-        return self.to_bytes(self.value, self._n_bits)
-
-
-
 class Float(Number):
     # https://en.wikipedia.org/wiki/Single-precision_floating-point_format
     # https://docs.python.org/3/library/struct.html
 
     N_BITS_A = 9
     N_BITS_B = 23
+    LIMIT = 1 << (N_BITS_A - 1)
 
 
     def __init__(self, value, n_bits_A = None, n_bits_B = None):
@@ -129,47 +77,56 @@ class Float(Number):
         self._n_bits_A = n_bits_A or self.N_BITS_A
         self._n_bits_B = n_bits_B or self.N_BITS_B
 
-        SignedInteger.limit_guard(value, self._n_bits_A)
+        self.limit_guard(value, self._n_bits_A)
 
 
     @classmethod
     def from_bits(cls, bits, n_bits_A = None, n_bits_B = None):
-        n_bits_A = n_bits_A or cls.N_BITS_A
-        n_bits_B = n_bits_B or cls.N_BITS_B
         return cls(cls.bits_to_value(bits, n_bits_A, n_bits_B), n_bits_A, n_bits_B)
 
 
     @classmethod
     def from_bytes(cls, AB_bytes, n_bits_A = None, n_bits_B = None):
-        n_bits_A = n_bits_A or cls.N_BITS_A
-        n_bits_B = n_bits_B or cls.N_BITS_B
         return cls(cls.bytes_to_value(AB_bytes, n_bits_A, n_bits_B), n_bits_A, n_bits_B)
 
 
     @classmethod
-    def bits_to_value(cls, bits, n_bits_A, n_bits_B):
+    def bits_to_value(cls, bits, n_bits_A = None, n_bits_B = None):
+        n_bits_A = n_bits_A or cls.N_BITS_A
+        n_bits_B = n_bits_B or cls.N_BITS_B
+
+        sign_mask = 1 << (n_bits_A + n_bits_B - 1)
         denominator = 1 << n_bits_B
-        return SignedInteger.bits_to_value(bits, n_bits_A + n_bits_B) / denominator
+        numerator = (bits & (sign_mask - 1)) - \
+                    (bits & (sign_mask - 0))
+
+        return numerator / denominator
 
 
     @classmethod
-    def bytes_to_value(cls, AB_bytes, n_bits_A, n_bits_B):
+    def bytes_to_value(cls, AB_bytes, n_bits_A = None, n_bits_B = None):
         bits = int.from_bytes(AB_bytes, byteorder = 'big', signed = False)
         return cls.bits_to_value(bits, n_bits_A, n_bits_B)
 
 
     @classmethod
-    def to_bits(cls, value, n_bits_A, n_bits_B):
-        SignedInteger.limit_guard(value, n_bits_A)
+    def to_bits(cls, value, n_bits_A = None, n_bits_B = None):
+        n_bits_A = n_bits_A or cls.N_BITS_A
+        n_bits_B = n_bits_B or cls.N_BITS_B
+
+        cls.limit_guard(value, n_bits_A)
 
         denominator = 1 << n_bits_B
         numerator = int(value * denominator)
 
-        return SignedInteger.to_bits(numerator, n_bits_A + n_bits_B)
+        return numerator + 2 ** (n_bits_A + n_bits_B) if numerator < 0 else numerator
 
 
     @classmethod
-    def to_bytes(cls, value, n_bits_A, n_bits_B):
+    def to_bytes(cls, value, n_bits_A = None, n_bits_B = None):
+        n_bits_A = n_bits_A or cls.N_BITS_A
+        n_bits_B = n_bits_B or cls.N_BITS_B
+
         bits = cls.to_bits(value, n_bits_A, n_bits_B)
         return bits.to_bytes(length = ceil((n_bits_A + n_bits_B) / 8),
                              byteorder = 'big',
@@ -186,3 +143,13 @@ class Float(Number):
         return self.to_bytes(self.value, self._n_bits_A, self._n_bits_B)
 
 
+
+class SignedInteger(Float):
+    N_BITS_A = 32
+    N_BITS_B = 0
+    LIMIT = 1 << (N_BITS_A - 1)
+
+
+    @classmethod
+    def bits_to_value(cls, bits, n_bits_A = None, _ = None):
+        return int(super().bits_to_value(bits, n_bits_A, None))
